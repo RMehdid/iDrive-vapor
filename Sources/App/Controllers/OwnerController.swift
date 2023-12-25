1f0906eef3de2ca2d0c08fe7a43a6b50f33f795e
Samy Abderraouf Mehdid <sr.mehdid@gmail.com>
2023-12-25 15:29:55 +0100

    work on owner api

--- Sources/App/Controllers/OwnerController.swift
+++ Sources/App/Controllers/OwnerController.swift
@@ -13,6 +13,11 @@ struct OwnerController: RouteCollection {
         let owners = routes.grouped("owners")
         owners.get(use: index)
         owners.post(use: create)
+        owners.put(use: update)
+        
+        owners.group(":owner_id") { owner in
+            owner.delete(use: delete)
+        }
     }
     
     func index(req: Request) async throws -> [Owner] {
@@ -22,5 +27,28 @@ struct OwnerController: RouteCollection {
     func create(req: Request) async throws -> HTTPStatus {
         let owners = try req.content.decode(Owner.self)
         return try await owners.save(on: req.db).transform(to: .ok).get()
+    }
+    
+    func update(req: Request) throws -> EventLoopFuture<HTTPStatus> {
+        let owner = try req.content.decode(Owner.self)
+        
+        return Owner.find(owner.id, on: req.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap {
+                $0.firstname = owner.firstname
+                $0.lastname = owner.lastname
+                $0.email = owner.email
+                $0.phone = owner.phone
+                $0.profileImageUrl = owner.profileImageUrl
+                $0.rating = owner.rating
+                return $0.update(on: req.db).transform(to: .ok)
+            }
+    }
+    
+    func delete(req: Request) throws -> EventLoopFuture<HTTPStatus> {
+        Owner.find(req.parameters.get("owner_id"), on: req.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap { $0.delete(on: req.db)}
+            .transform(to: .ok)
     }
 }
