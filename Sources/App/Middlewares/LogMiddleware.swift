098645e70e98764548d50ff450737c1eaf432fd9
Samy Abderraouf Mehdid <rmehdid@harbineldjazair.com>
2024-01-25 11:14:16 +0100

    added middlewares

---
+++ Sources/App/Middlewares/LogMiddleware.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Samy Mehdid on 25/1/2024.
+//
+
+import Vapor
+
+struct LogMiddleware: AsyncMiddleware {
+    func respond(to request: Request, chainingTo next: AsyncResponder) async throws -> Response {
+        print("LOG MIDDLEWARE")
+        
+        return try await next.respond(to: request)
+    }
+}
